bb780da15fcf19b2c901964fd27420055e60aa39
Alfredo Cuevas <alfredoecuevas@icloud.com>
2026-01-27 15:35:21 -0500

    Add files via upload

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,39 @@
+<?php
+    $inData = getRequestInfo();
+    
+    $contact = $inData["ContactId"];
+    $userId = $inData["OwnerId"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
+    if($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT into Contacts (OwnerId, ContactId) VALUES(?,?)"); //VERIFY TABLE NAME Contact, contact or CONTACT??
+        $stmt->bind_param("ii", $userId, $contact );
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+   function sendResultInfoAsJson ( $obj )
+   {
+        header('Content-type: application/json');
+        echo $obj;
+   } 
+
+   function returnWithError( $err )
+   {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+   }
+
+?>
