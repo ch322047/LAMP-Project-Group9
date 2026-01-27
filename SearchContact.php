c472a03936e795c44eaae9fd6c252f02d109a408
Christopher Otto <155487849+DKINGRHINO@users.noreply.github.com>
2026-01-27 04:28:46 -0500

    SearchContact.php & UpdateContact.php Completed
    UpdateContact.php and SearchContact.php are done. They've been tested with Postman. Here's what json they expect to be sent:
      By the way, the entries in these json files can be in any order as long as they are present. If an entry is missing, then an error occurs.
      UpdateContact.php:
        {
            "newFirstName" : "[name here]",
            "newLastName" : "[name here]",
            "newPhone" : "[phone number here]",
            "newEmail" : "[email here]",
            "newLastName" : [contactId here],
            "newLastName" : [userId (OwnderId) here]
        }
    
      SearchContact.php:
        {
            "userId" : [userId here],
            "search" : "[search here]"
        }
    
    Here's what errors can occur:
      I'll give the exact error message followed by an explanation of why it occurs.
      UpdateContact.php:
        'Update Failed! Failed to connect to MySQL: [reason here, if any]'
            Reason is exactly as stated.
        'Update Failed! Missing userId.'
            Reason is exactly as stated (userId needs to be in the json and set to an int).
        'Update Failed! Missing contact information.'
            Reason is exactly as stated (one or more of the contact information is missing from json).
        'Update Failed! Invalid userId / contactId combination.'
            Reason is exactly as stated.
        'Update Failed!'
            Reason is that stmt->execute() failed for an unknown reason.
      SearchContact.php:
        'Search Failed! Failed to connect to MySQL: [reason here, if any]'
            Reason is exactly as stated.
        'No Records Found'
            Search returned no results.
    
    Functionality:
      UpdateContact.php takes all of the contact information above (whether you're changing all information, just one value, nothing, or anything in between) and sets the values in the table to the ones in the json request.
      SearchContact.php takes all of the info above (in the json) and searches through the Contacts table for your search query in FirstName or LastName and returns any results as an array of objects.
    
    Also, SearchContact.php behavior when you do an empty search ("") is to return all contacts belonging to the specified userId. Front end people can use this to fetch the contacts that should be displayed when nothing is in the search field, and then when you search the contacts are reduced to the ones that match the search criteria.

---
+++ SearchContact.php
@@ -0,0 +1,75 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	// Establish connection to database
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if( $conn->connect_error ) // Connection failed
+	{
+		returnWithError("Search Failed! Failed to connect to MySQL: " . $conn->connect_error);
+	}
+	else
+	{
+		// Setup a query to return all rows with FirstName or LastName for a given OwnerId
+		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? OR LastName like ?) and OwnerId = ?");
+		$searchText = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ssi", $searchText, $searchText, $inData["userId"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+
+		// Traverse the rows of results, saving each row and incrementing $searchCount appropriately
+		while($row = $result->fetch_assoc())
+		{
+			// Put a comma in between search results (rows)
+			if($searchCount > 0)
+			{
+				$searchResults .= ",";
+			}
+			
+			$searchCount++;
+			
+			$searchResults .= '{"FirstName" : "' . $row["FirstName"].'", 
+								"LastName" : "' . $row["LastName"].'", 
+								"Phone" : "' . $row["Phone"].'", 
+								"Email" : "' . $row["Email"].'"}';
+		}
+		
+		if($searchCount == 0) // No search results
+		{
+			returnWithError("No Records Found");
+		}
+		else
+		{
+			returnWithInfo($searchResults);
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+	function returnWithInfo($searchResults)
+	{
+		$retValue = '{"results": [' . $searchResults . '], "error": ""}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
