771ace6839d59ce0c7962f9c5ae43d4aaec573a2
Friederike123 <friederike1234567@gmail.com>
2026-02-02 20:16:24 -0500

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -15,9 +15,9 @@
     of the concepts.
     */
 
-    /*ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);*/
+    error_reporting(E_ALL);
 
 	//read JSON and turn it into php array	
 	$inData = getRequestInfo();
